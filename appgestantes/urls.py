e57056a124cdb552468424ccefbb3d15cf717c22
bgtyhn <juangamix@gmail.com>
2016-12-04 01:27:55 -0500

    correciones push

--- appgestantes/urls.py
+++ appgestantes/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:gestantes/appgestantes/urls.py
 from django.conf.urls import url
 
 from . import views
@@ -17,24 +16,4 @@ urlpatterns = [
     url(r'^editar_segundo_trimestre/$', views.editar_segundo_trimestre, name='editar_segundo_trimestre'),
     url(r'^editar_tercer_trimestre/$', views.editar_tercer_trimestre, name='editar_tercer_trimestre'),
     url(r'^editar_citas/$', views.editar_citas, name='editar_citas'),
-=======
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.Index.as_view(), name='index'),
-    url(r'^prioritarias/$', views.Prioritarias.as_view(), name='prioritarias'),
-    url(r'^completa/$', views.Completa.as_view(), name='completa'),
-    url(r'^pasadas/$', views.Pasadas.as_view(), name='pasadas'),
-    url(r'^nueva/$', views.Nueva.as_view(), name='nueva'),
-    url(r'^detalle/(?P<pk>[-\w]+)/$', views.Detalle.as_view(), name='detalle'),
-
-    url(r'^editar_general/$', views.editar_general, name='editar_general'),
-    url(r'^editar_primer_control/$', views.editar_primer_control, name='editar_primer_control'),
-    url(r'^editar_primer_trimestre/$', views.editar_primer_trimestre, name='editar_primer_trimestre'),
-    url(r'^editar_segundo_trimestre/$', views.editar_segundo_trimestre, name='editar_segundo_trimestre'),
-    url(r'^editar_tercer_trimestre/$', views.editar_tercer_trimestre, name='editar_tercer_trimestre'),
-    url(r'^editar_citas/$', views.editar_citas, name='editar_citas'),
->>>>>>> c9bceb7a064673e92bcd03124dbd076bbd9f059b:appgestantes/urls.py
 ]
---
+++ gestantes/appgestantes/forms.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+from django import forms
+from .models import *
+from django.utils.translation import ugettext_lazy as _
+
+errores_defecto = {
+    'required': 'Este campo se requiere',
+    'invalid': 'Ingresar un dato válido'
+}
+
+class ModelGestanteForm(forms.ModelForm):
+	class Meta:
+		model = Gestante
+		fields = ('nombre', 'fecha_ingreso_programa', 'fecha_nacimiento', 'identificacion', 'captacion', 'semana_ingreso', 'fecha_ultima_menstruacion', 'fecha_probable_parto', 'confiable')
+		error_messages = {
+			'nombre':{
+				'required' : _('Por favor llene el campo')
+			},
+			'fecha_ingreso_programa':{
+				'required' : _('Por favor llene el campo')
+			},
+			'fecha_nacimiento':{
+				'required' : _('Por favor seleccione una imagen'),
+				'invalid' : _('Por favor seleccione un archivo de imagen')
+			},
+			'identificacion':{
+				'required' : _('Por favor llene el campo')
+			},
+			'captacion':{
+				'required' : _('Por favor llene el campo')
+			},
+			'semana_ingreso':{
+				'required' : _('Por favor llene el campo')
+			},
+			'fecha_ultima_menstruacion':{
+				'required' : _('Por favor llene el campo')
+			},
+			'fecha_probable_parto':{
+				'required' : _('Por favor llene el campo')
+			},
+			'confiable':{
+				'required' : _('Por favor llene el campo')
+			},
+
+		}
+
+class ModelPrimerControlForm(forms.ModelForm):
+	class Meta:
+		model = PrimerControl
+		fields = ('fecha_paraclinicos', 'micronutrientes', 'pretest_fecha', 'fecha_postest', 'iami', 'odontologia_fecha', 'citologia_fecha', 'citologia_resultado', 'DPTa')
+		error_messages = {
+			'fecha_paraclinicos':{
+				'required' : _('Por favor llene el campo')
+			},
+			'micronutrientes':{
+				'required' : _('Por favor seleccione una imagen'),
+				'invalid' : _('Por favor seleccione un archivo de imagen')
+			},
+			'pretest_fecha':{
+				'required' : _('Por favor llene el campo')
+			},
+			'fecha_postest':{
+				'required' : _('Por favor llene el campo')
+			},
+			'iami':{
+				'required' : _('Por favor llene el campo')
+			},
+			'odontologia_fecha':{
+				'required' : _('Por favor llene el campo')
+			},
+			'citologia_fecha':{
+				'required' : _('Por favor llene el campo')
+			},
+			'citologia_resultado':{
+				'required' : _('Por favor llene el campo')
+			},
+			'DPTa':{
+				'required' : _('Por favor llene el campo')
+			},
+		}
+
+
+class GestanteForm(forms.Form):
+	nombre = forms.CharField(error_messages = errores_defecto)
+	fecha_ingreso_programa = forms.DateField(error_messages = errores_defecto) #verificar
+	fecha_nacimiento = forms.DateField(error_messages = errores_defecto)
+	identificacion = forms.CharField(error_messages = errores_defecto)
+	captacion = forms.CharField(error_messages = errores_defecto)
+	semana_ingreso = forms.CharField(error_messages = errores_defecto) #verificar solo numero
+	fecha_ultima_menstruacion = forms.DateField(error_messages = errores_defecto) #verificar <= hoy
+	fecha_probable_parto = forms.DateField(error_messages = errores_defecto)
+	confiable = forms.CharField(error_messages = errores_defecto, required = False)
+	fecha_paraclinicos = forms.DateField(error_messages = errores_defecto) # > hoy
+	micronutrientes = forms.CharField(error_messages = errores_defecto, required = False)
+	pretest_fecha = forms.DateField(error_messages = errores_defecto) # > hoy
+	fecha_postest = forms.DateField(error_messages = errores_defecto) # > hoy
+	iami = forms.CharField(error_messages = errores_defecto, required = False)
+	odontologia_fecha = forms.DateField(error_messages = errores_defecto)
+	citologia_fecha = forms.DateField(error_messages = errores_defecto)
+	citologia_resultado = forms.CharField(error_messages = errores_defecto)
+	DPTa = forms.CharField(error_messages = errores_defecto, required = False)
+
+class EditarGeneralForm(forms.Form):
+	fecha_probable_parto = forms.DateField(error_messages = errores_defecto)
+	identificacion = forms.CharField(error_messages = errores_defecto)
+	confiable = forms.CharField(error_messages = errores_defecto, required = False)
+	fecha_nacimiento = forms.DateField(error_messages = errores_defecto)
+	semana_ingreso = forms.CharField(error_messages = errores_defecto) #verificar solo numero
+	captacion = forms.CharField(error_messages = errores_defecto)
+	fecha_ultima_menstruacion = forms.DateField(error_messages = errores_defecto) #verificar <= hoy
+	fecha_ingreso_programa = forms.DateField(error_messages = errores_defecto) #verificar
