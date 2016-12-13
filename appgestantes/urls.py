42dc6da67ffdf489bcb1b728c25b365c81ed8950
bgtyhn <juangamix@gmail.com>
2016-12-13 02:48:19 -0500

    Form TercerTrimestre V0.001

--- appgestantes/urls.py
+++ appgestantes/urls.py
@@ -14,6 +14,6 @@ urlpatterns = [
     url(r'^editar_primer_control/(?P<gestante>[-\w]+)/(?P<control>[-\w]+)/$', views.EditarPrimerControl.as_view(), name='editar_primer_control'),
     url(r'^editar_primer_trimestre/(?P<gestante>[-\w]+)/$', views.EditarPrimerTrimestre.as_view(), name='editar_primer_trimestre'),
     url(r'^editar_segundo_trimestre/(?P<gestante>[-\w]+)/$', views.EditarSegundoTrimestre.as_view(), name='editar_segundo_trimestre'),
-    url(r'^editar_tercer_trimestre/$', views.editar_tercer_trimestre, name='editar_tercer_trimestre'),
+    url(r'^editar_tercer_trimestre/(?P<gestante>[-\w]+)/$', views.EditarTercerTrimestre.as_view(), name='editar_tercer_trimestre'),
     url(r'^editar_citas/$', views.editar_citas, name='editar_citas'),
 ]
